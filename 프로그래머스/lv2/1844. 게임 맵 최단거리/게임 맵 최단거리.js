3c54ecf758532e3ece8facec66a5e2115511a22e
suld2495 <42727909+suld2495@users.noreply.github.com>
2023-02-08 00:17:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.12 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,25 @@
+function solution(maps) {
+    let directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+    let queue = [[0, 0, 1]];
+    const n = maps[0].length - 1;
+    const m = maps.length - 1;
+    let min = Infinity;
+    
+    while (queue.length) {
+        const [x, y, count] = queue.shift();
+                
+        if (x === n && y === m) return count;
+        
+        for ([dx, dy] of directions) {
+            const nx = x + dx;
+            const ny = y + dy;
+            
+            if (nx < 0 || ny < 0 || nx > n || ny > m || !maps[ny][nx]) continue;
+            
+            maps[ny][nx] = 0;
+            queue.push([nx, ny, count + 1]);
+        }
+    }
+    
+    return -1;
+}
