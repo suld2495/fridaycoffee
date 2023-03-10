032c60ce052b1ebe098aa848f84dbe3400747ef4
suld2495 <42727909+suld2495@users.noreply.github.com>
2023-03-10 21:26:46 +0900

    [level 2] Title: 양궁대회, Time: 2.83 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.js
@@ -13,7 +13,7 @@ function solution(n, info) {
                 } else if (v < list[i]) {
                     acc += 10 - i;
                 }
-                
+
                 return acc;
             }, 0); 
 
@@ -21,32 +21,30 @@ function solution(n, info) {
               max = score;
               maxList = list;
             } else if (score === max) {
-              for (let i = info.length - 1; i >= 0; i -= 1) {
-                if (maxList[i] < list[i]) {
-                  maxList = list;
-                  break;
-                }
-              }
+              const isMax = [...list]
+                .sort((a, b) => b - a)
+                .some((v, i) => maxList[i] < v);
+                
+              if (isMax) {
+                maxList = list;
+              }  
             }
           
             continue;
         }
         
-        for (let i = index; i < info.length; i += 1) {
-            if (info[i] < n - count) {
-                if (i === info.length - 1) {
-                  const newList = [...list];
-                  newList[i] = n - count;
-                  queue.push([newList, n, i + 1]);
-                } else {
-                  queue.push([list, count, i + 1]);
-                  queue.push([list.map((v, index) => index === i ? info[i] + 1 : v), count + info[i] + 1, i + 1]);
-                }
-            } else if (i === info.length - 1) {
-              const newList = [...list];
-              newList[i] = n - count;
-              queue.push([newList, n, i + 1]);
-            }
+        if (index === 10) {
+          const newList = [...list];
+          newList[index] = n - count;
+          queue.push([newList, n, index + 1]);
+        } else {
+          queue.push([list, count, index + 1]);
+        
+          if (info[index] < n - count) {
+            const newList = [...list];
+            newList[index] = info[index] + 1;
+            queue.push([newList, count + info[index] + 1, index + 1]); 
+          }  
         }
     }
 
