0b83d5150deeed24094164a654a1a687958d004e
suld2495 <42727909+suld2495@users.noreply.github.com>
2023-03-16 20:06:12 +0900

    [level 1] Title: 모의고사, Time: 2.64 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -0,0 +1,24 @@
+function calculate(answers, list) {
+    return answers.filter((v, i) => v === list[i % list.length]).length;
+}
+
+function solution(answers) {
+    const 수포자1 = [1, 2, 3, 4, 5];
+    const 수포자2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const 수포자3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    let max = 0;
+    
+    return [수포자1, 수포자2, 수포자3].reduce((acc, 수포자, i) => {
+        const result = calculate(answers, 수포자);     
+
+        if (max < result) {
+            max = result;
+            acc = [i + 1];
+        } else if (max === result) {
+            acc.push(i + 1);
+        }
+        
+        return acc;
+    }, []);
+}
