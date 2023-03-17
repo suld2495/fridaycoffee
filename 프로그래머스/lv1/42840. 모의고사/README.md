7176d4d6c3b9b0f0f79042c30ba022aa6496c070
suld2495 <42727909+suld2495@users.noreply.github.com>
2023-03-17 10:28:42 +0900

    [level 1] Title: 모의고사, Time: 1.73 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/README.md
+++ 프로그래머스/lv1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 35.9 MB, 시간: 2.64 ms
+메모리: 36 MB, 시간: 1.73 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -6,9 +6,9 @@ function solution(answers) {
     const 수포자1 = [1, 2, 3, 4, 5];
     const 수포자2 = [2, 1, 2, 3, 2, 4, 2, 5];
     const 수포자3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
+
     let max = 0;
-    
+
     return [수포자1, 수포자2, 수포자3].reduce((acc, 수포자, i) => {
         const result = calculate(answers, 수포자);     
 
@@ -18,7 +18,7 @@ function solution(answers) {
         } else if (max === result) {
             acc.push(i + 1);
         }
-        
+
         return acc;
     }, []);
 }
