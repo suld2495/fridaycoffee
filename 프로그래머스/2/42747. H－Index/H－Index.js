29326d04df0d2e41fcf920f2ebfeae7827e8942e
suld2495 <42727909+suld2495@users.noreply.github.com>
2024-03-29 21:32:19 +0900

    [level 2] Title: H-Index, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,15 +1,15 @@
 function solution(list) {
     list.sort((a, b) => b - a);
     
+    if (list.at(-1) >= list.length) {
+        return list.length;
+    }
+    
     for (let i = list[0]; i >= 0; i -= 1) {
         const h = list.findIndex((h) => h < i);
         
         if (h >= i) {
             return i;
-        }
-        
-        if (list.at(-1) && h === -1) {
-            return list.length;
         }
     }
     
