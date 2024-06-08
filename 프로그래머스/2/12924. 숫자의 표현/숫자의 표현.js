ea1d91b60bf0219690af4efc9800f193952cf80a
suld2495 <42727909+suld2495@users.noreply.github.com>
2024-06-08 18:12:10 +0900

    [level 2] Title: 숫자의 표현, Time: 2.42 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,9 +1,24 @@
 function solution(num) {
-    var answer = 0;
-    for (var i = 1; i <= num; i++) {
-        if ((num % i == 0) && (i % 2 == 1)) {
-            answer++;
-        }
-    }
-    return answer;
+	let count = 0;
+	
+	function dfs(i, sum, check) {
+		if (sum === num) {
+			count += 1;
+			return;
+		}
+		
+		if (i > num 
+			|| sum > num 
+			|| (sum + i) > num
+			|| (!check && (i * 2) > num)) {
+			return;
+		}
+		
+		dfs(i + 1, sum + i, true);
+		!check && dfs(i + 1, 0, false);            
+	}
+	
+	dfs(1, 0, false);
+	
+	return count + 1;
 }
