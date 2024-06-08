2af7253a34206d8126c8dff7fdef2969863bbfde
suld2495 <42727909+suld2495@users.noreply.github.com>
2024-06-08 18:16:54 +0900

    [level 2] Title: 숫자의 표현, Time: 1.48 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,13 +1,13 @@
-function solution(n) {
+function solution(num) {
   let count = 0;
 
-  for (let i = 1; i <= Math.abs(n); i += 1) {
+  for (let i = 1; i <= num; i += 1) {
     let sum = 0;
 
-    for (let j = i; j <= n && sum < n; j += 1) {
+    for (let j = i; j <= num && sum < num; j += 1) {
       sum += j;
 
-      if (sum === n) {
+      if (sum === num) {
         count += 1;
       }
     }
