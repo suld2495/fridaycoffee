ee180ec746518586ba33a653d59513034fcf8ffd
suld2495 <42727909+suld2495@users.noreply.github.com>
2025-05-13 21:44:49 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 544 ms, Memory: 88584 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
 let [[N, M], ...inputs] = fs.readFileSync('/dev/stdin').toString().split('\n').map((v) => v.split(' ').map(Number));
 
-const table = inputs.splice(0, N)
+const table = inputs.splice(0, N);
 const sumList = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
 
-for (let i = 1; i < N + 1; i += 1) {
-  for (let j = 1; j < N + 1; j += 1) {
-    sumList[i][j] = sumList[i][j - 1] + sumList[i - 1][j] + table[i - 1][j - 1] - sumList[i - 1][j - 1];
+for (let i = 0; i < N; i += 1) {
+  for (let j = 0; j < N; j += 1) {
+    sumList[i + 1][j + 1] = table[i][j] + sumList[i][j + 1] + sumList[i + 1][j] - sumList[i][j];
   }
 }
 
-const output = [];
+const list = [];
 
 for (let i = 0; i < M; i += 1) {
   const [x1, y1, x2, y2] = inputs[i];
-  output.push(sumList[x2][y2] - sumList[x1 - 1][y2] - sumList[x2][y1 - 1] + sumList[x1 - 1][y1 - 1]);
+  const sum = sumList[x2][y2] - sumList[x1 - 1][y2] - sumList[x2][y1 - 1] + sumList[x1 - 1][y1 - 1]
+  list.push(sum);
 }
 
-console.log(output.join('\n'));
+console.log(list.join('\n'));
